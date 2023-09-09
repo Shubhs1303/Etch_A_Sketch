7d5b0afb138f3395e02a14520ce3d7b8f825129a
Shubham <117352363+Shubhs1303@users.noreply.github.com>
2023-09-09 23:27:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function createPixels(size) {
+    let container = document.querySelector(".container");
+    for (currentPixel = 0; currentPixel < size * size; currentPixel++) {
+      let pixel = document.createElement("div");
+      pixel.setAttribute("class", "pixel");
+      pixel.addEventListener("pointerover", addRgb);
+      container.appendChild(pixel);
+    }
+  }
+  
+  createPixels(16);
+  
+  function addRgb(e) {
+    e.target.style = `background-color: rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+    e.target.removeEventListener("pointerover", addRgb);
+  }
+  
+  function resetContainer() {
+    let allPixels = document.querySelectorAll(".pixel");
+    allPixels.forEach(pixel => pixel.parentNode.removeChild(pixel));
+  }
